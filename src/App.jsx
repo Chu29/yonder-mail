b01ed988f6fe8abe1ed4405b26b9d01bd96aad4d
Chu A. <chu.amk22@gmail.com>
2026-01-30 18:15:40 +0100

    feat: Add route for LoginPage component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
