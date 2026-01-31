b8f94bba11eb143bd58436bf0cdfc064832fb8d0
Chu A. <chu.amk22@gmail.com>
2026-01-31 14:48:46 +0100

    feat: Implement RecordingPage layout with components for header, webcam preview, control panel, and privacy notice

--- src/pages/Recording/RecordingPage.jsx
+++ src/pages/Recording/RecordingPage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import RecordingHeader from "./components/RecordingHeader";
+import WebcamPreview from "./components/WebcamPreview";
+import ControlPanel from "./components/ControlPanel";
+import PrivacyNotice from "./components/PrivacyNotice";
 
 const RecordingPage = () => {
-  return <div>RecordingPage</div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <RecordingHeader />
+
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-8">
+        {/* Webcam Preview */}
+        <div className="mb-8">
+          <WebcamPreview />
+        </div>
+
+        {/* Control Buttons */}
+        <ControlPanel />
+
+        {/* Privacy Notice */}
+        <PrivacyNotice />
+      </div>
+    </div>
+  );
 };
 
 export default RecordingPage;
