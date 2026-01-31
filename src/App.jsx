414de10b9c440f1ad2c88eb6f360bd9cea40c9d9
Chu A. <chu.amk22@gmail.com>
2026-01-31 13:34:15 +0100

    feat: Add route for RecordingPage component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import RecordingPage from "./pages/RecordingPage";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/recording" element={<RecordingPage />} />
       </Routes>
     </BrowserRouter>
   );
