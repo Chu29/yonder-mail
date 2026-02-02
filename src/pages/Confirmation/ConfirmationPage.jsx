dec8cb40effef368c338ebf161266aa59de78d35
Chu A. <chu.amk22@gmail.com>
2026-02-02 10:17:00 +0100

    feat: Add ConfirmationPage component to display message scheduling confirmation

---
+++ src/pages/Confirmation/ConfirmationPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ConfirmationHeader from "./components/ConfirmationHeader";
+import ConfirmationContent from "./components/ConfirmationContent";
+import Footer from "../Home/components/Footer";
+
+const ConfirmationPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <ConfirmationHeader />
+
+      <div className="flex-1 flex items-center justify-center px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
+        <ConfirmationContent />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default ConfirmationPage;
