302bc7e8de9739201a8b200bea592389d307b87d
Chu A. <chu.amk22@gmail.com>
2026-02-08 11:01:38 +0100

    fix: Update route path for MyMessagesPage from '/my-messages' to '/messages'

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/preview" element={<PreviewPage />} />
         <Route path="/schedule" element={<SchedulePage />} />
         <Route path="/confirmation" element={<ConfirmationPage />} />
-        <Route path="/my-messages" element={<MyMessagesPage />} />
+        <Route path="/messages" element={<MyMessagesPage />} />
       </Routes>
     </BrowserRouter>
   );
