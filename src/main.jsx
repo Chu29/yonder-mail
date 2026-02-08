e998ad0014eb7db2644091ffc31cda628bdadcbd
Chu A. <chu.amk22@gmail.com>
2026-02-08 11:03:15 +0100

    feat: wrap App component with AuthProvider, MessageProvider, and RecordingProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext";
+import { RecordingProvider } from "./context/RecordingContext";
+import { MessageProvider } from "./context/MessageContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <MessageProvider>
+        <RecordingProvider>
+          <App />
+        </RecordingProvider>
+      </MessageProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
