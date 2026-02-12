7fff3b8a352f0f123dd8c19c3f58edb8400d7910
Chu A. <chu.amk22@gmail.com>
2026-02-12 17:34:45 +0100

    refactor: replace ConfirmationHeader with AppHeader in ConfirmationPage

--- src/pages/Confirmation/ConfirmationPage.jsx
+++ src/pages/Confirmation/ConfirmationPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ConfirmationHeader from "./components/ConfirmationHeader";
+import AppHeader from "../../components/AppHeader";
 import ConfirmationContent from "./components/ConfirmationContent";
 import Footer from "../Home/components/Footer";
 
 const ConfirmationPage = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <ConfirmationHeader />
+      <AppHeader />
 
       <div className="flex-1 flex items-center justify-center px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
         <ConfirmationContent />
