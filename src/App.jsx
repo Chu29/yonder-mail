fa72c7fd2bc2839047673b2f82354364c6fdea79
Chu A. <chu.amk22@gmail.com>
2026-02-16 00:48:50 +0100

    feat: add About and FAQ pages to routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,28 @@ import PreviewPage from "./pages/Preview/PreviewPage";
 import SchedulePage from "./pages/Schedule/SchedulePage";
 import ConfirmationPage from "./pages/Confirmation/ConfirmationPage";
 import MyMessagesPage from "./pages/MyMessages/MyMessagesPage";
+import AboutPage from "./pages/About/AboutPage";
+import FAQPage from "./pages/FAQ/FAQPage";
+import ToastContainer from "./components/ToastContainer";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/recording" element={<RecordingPage />} />
-        <Route path="/preview" element={<PreviewPage />} />
-        <Route path="/schedule" element={<SchedulePage />} />
-        <Route path="/confirmation" element={<ConfirmationPage />} />
-        <Route path="/messages" element={<MyMessagesPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/recording" element={<RecordingPage />} />
+          <Route path="/preview" element={<PreviewPage />} />
+          <Route path="/schedule" element={<SchedulePage />} />
+          <Route path="/confirmation" element={<ConfirmationPage />} />
+          <Route path="/messages" element={<MyMessagesPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/faqs" element={<FAQPage />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 };
 
