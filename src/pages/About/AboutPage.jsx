7bae3352efd9739d468fb127cfb722544f76804d
Chu A. <chu.amk22@gmail.com>
2026-02-16 01:05:25 +0100

    feat: add Footer component to AboutPage for improved layout

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,11 +1,13 @@
 import AboutHeader from "./components/AboutHeader";
 import AboutContent from "./components/AboutContent";
+import Footer from "../../components/Footer";
 
 const AboutPage = () => {
   return (
     <>
       <AboutHeader />
       <AboutContent />
+      <Footer />
     </>
   );
 };
